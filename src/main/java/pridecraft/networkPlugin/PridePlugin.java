8e57648eb04bdb1c8a5369bddf20c7aa77563edf
Notarin <424c414e4b@gmail.com>
2023-01-29 17:55:50 -0500

    Initial plugin boilerplate

---
+++ src/main/java/pridecraft/networkPlugin/PridePlugin.java
@@ -0,0 +1,10 @@
+package pridecraft.networkPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PridePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+}
