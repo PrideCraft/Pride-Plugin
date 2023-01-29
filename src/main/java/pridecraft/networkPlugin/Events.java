3c708ba2f637c493850f8c960a173753c17ffeb9
Notarin <424c414e4b@gmail.com>
2023-01-29 18:57:32 -0500

    Added config handling

---
+++ src/main/java/pridecraft/networkPlugin/Events.java
@@ -0,0 +1,12 @@
+package pridecraft.networkPlugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class Events implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event) {
+    }
+
+}
--- src/main/java/pridecraft/networkPlugin/PridePlugin.java
+++ src/main/java/pridecraft/networkPlugin/PridePlugin.java
@@ -1,10 +1,33 @@
 package pridecraft.networkPlugin;
 
+import com.tchristofferson.configupdater.ConfigUpdater;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class PridePlugin extends JavaPlugin {
+import java.io.File;
+import java.io.IOException;
+
+public final class PridePlugin extends JavaPlugin implements Listener {
+
+    final FileConfiguration config = this.getConfig();
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        //The config needs to exist before using the updater
+        File configFile = new File(getDataFolder(), "config.yml");
+
+        try {
+            ConfigUpdater.update(this, "config.yml", configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        reloadConfig();
+        getServer().getPluginManager().registerEvents(new Events(), this);
+        if (config.getBoolean("startup-message")) {
+            System.out.println("Pride Network Started!");
+        }
     }
 }
